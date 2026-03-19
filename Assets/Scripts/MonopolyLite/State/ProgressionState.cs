d6114e3fe03bb69ab59e94a784e0e459ef0aad4f
Mehmet Can Ertuzun <mcertuzun@gmail.com>
2026-03-19 22:01:16 +0000

    feat(phase2): add ProgressionState with milestone/multiplier tracking

---
+++ Assets/Scripts/MonopolyLite/State/ProgressionState.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace MonopolyLite.State
+{
+    public class ProgressionState
+    {
+        public int CurrentBoardIndex { get; set; }
+        public int LoginStreak { get; set; }
+        public string LastLoginDate { get; set; }
+        public long LastRegenTicks { get; set; }
+        public int DiceRegenSeconds { get; set; }
+        public HashSet<int> ClaimedMilestones { get; private set; }
+        public List<int> UnlockedMultipliers { get; private set; }
+
+        public ProgressionState(int diceRegenSeconds = 300)
+        {
+            CurrentBoardIndex = 0;
+            LoginStreak = 0;
+            LastLoginDate = null;
+            LastRegenTicks = 0;
+            DiceRegenSeconds = diceRegenSeconds;
+            ClaimedMilestones = new HashSet<int>();
+            UnlockedMultipliers = new List<int> { 1 };
+        }
+
+        public bool IsMultiplierUnlocked(int multiplier)
+        {
+            return UnlockedMultipliers.Contains(multiplier);
+        }
+    }
+}
